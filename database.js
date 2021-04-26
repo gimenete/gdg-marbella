9d7fa5590678e2fee8ca03ba8d57bada62d2bf5e
Alberto Gimeno <gimenete@github.com>
2021-04-26 14:14:41 +0200

    Test db

--- database.js
+++ database.js
@@ -2,6 +2,7 @@ const { Client } = require('pg')
 const client = new Client()
 client.connect()
 client.query('SELECT version()', (err, res) => {
-  console.log(err ? err.stack : res.rows[0].version)
+  if (err) throw err
+  console.log(res.rows[0].version)
   client.end()
 })
